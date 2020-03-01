e48b73ed93fb40946f00e21293cdd4ed3375a03d
Peter Cai <peter@typeblog.net>
2020-03-01 10:12:48 +0800

    picasso: create symlink from libbluetooth to libbluetooth_qti
    * Vendor props calls for libbluetooth_qti but apparently it does not
      work at all.

--- Android.mk
+++ Android.mk
@@ -7,6 +7,16 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),picasso)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+LIBBLUETOOTH_QTI_SYMLINKS := $(TARGET_OUT)/lib64/libbluetooth_qti.so
+$(LIBBLUETOOTH_QTI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating libbluetooth_qti symlink"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) ln -sf /system/lib64/libbluetooth.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(LIBBLUETOOTH_QTI_SYMLINKS)
 endif
