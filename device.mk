a2c66cf0997803591a59590d7eb3e15858f3735f
Peter Cai <peter@typeblog.net>
2020-03-02 16:18:26 +0800

    picasso:introduce lineage-sdk overlays

--- device.mk
+++ device.mk
@@ -80,7 +80,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     NotchNoFillOverlay
