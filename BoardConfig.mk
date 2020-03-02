c91cbfdddf1b05e848e3553335ea2c74c859d43f
Peter Cai <peter@typeblog.net>
2020-03-02 16:53:40 +0800

    picasso: Set TARGET_PROVIDES_QTI_TELEPHONY_JAR
    * Though this is currently unused in LOS 17.1, it may be used when the
      ad-hoc qti-telephony-common implementation gets done at some point.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,9 @@ TARGET_USES_MKE2FS := true
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 
+# Telephony
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+
 # Treble
 BOARD_VNDK_VERSION := current
 
