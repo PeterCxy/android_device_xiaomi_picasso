df5a7b53382fcbd9a388209b97744b982de76a87
Peter Cai <peter@typeblog.net>
2020-03-02 17:51:07 +0800

    picasso: sepolicy: Pull in QCOM generic platform sepolicy
    * This makes part of our work easier.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,14 @@ TARGET_USES_MKE2FS := true
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
+
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
