58c4bc4597ea01fa925c1c48d1f4466aa287541a
Peter Cai <peter@typeblog.net>
2020-03-06 21:09:34 +0800

    picasso: recovery: Make symlinks for dynamic partitions
    * For compatibility with some custom mods (However Magisk still needs a
      patch for failed `mount --move`)

--- recovery/root/init.recovery.qcom.sh
+++ recovery/root/init.recovery.qcom.sh
@@ -1,3 +1,10 @@
 #!/system/bin/sh
 # Make recovery permissive
 setenforce 0
+# Wait for some time to avoid us being overridden
+sleep 5
+# Make symlinks from /dev/block/mapper to /dev/block/bootdevice/by-name
+# For compatibility with mods like Magisk and OpenGapps
+# (vendor and system should be more than enough)
+ln -sf /dev/block/mapper/system /dev/block/bootdevice/by-name/system
+ln -sf /dev/block/mapper/vendor /dev/block/bootdevice/by-name/vendor
